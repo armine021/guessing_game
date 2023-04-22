14f4d0f0f197cc6f92737bc8927dcfe38c9ad105
Nadine Scherer <armine021@gmail.com>
2023-04-22 15:29:38 -0700

    add guess conversion and comparison

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // import statement
 // std = standard library, io = input/output
 use std::io;
+// compare values and return variants Less, Greater, or Equal
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -14,7 +16,7 @@ fn main() {
 
     // The Book uses rand library, but fastrand is smaller and faster
     // creates immutable random number between 1 and 100, inclusive on lower and upper bounds
-    let secret_number = fastrand::i32(1..=100);
+    let secret_number = fastrand::u32(1..=100);
 
     // for initial dev, secret_number will be printed so we can predict behavior better
     println!("The secret number is {secret_number}");
@@ -27,7 +29,7 @@ fn main() {
         // "&mut guess" argument tells where to store the input
         // "&" indicates a reference to avoid copying data into memory multiple times
         // referenes are immutable by default (like variables), so use &mut to make mutable
-        // returns a Result to the handle of "Ok" and the generated value or "Err" and why it failed
+        // returns a Result variant to the handle of "Ok" and the generated value or "Err" and why it failed
         .read_line(&mut guess)
         // could have kept this as one line, but separate for readability
         // if Result is "Ok", the generated value is returned
@@ -35,7 +37,25 @@ fn main() {
         // for now, keep error handling simple and just print an error statement
         .expect("Failed to read line");
 
+    // Rust allows shadowing, so multiple variables of the same name can exist as different types
+    let guess: u32 = guess
+        // eliminate whitespace at beginning and end
+        .trim()
+        // convert string to another type
+        .parse()
+        // error message
+        .expect("Please type a number!");
+
     // {} placeholder for the value of the variable
     // for multiple placeholders, format is prinln!("x = {} and y = {}", x, y);
     println!("You guessed: {guess}");
+
+    // compare guess value and reference value for secret_number
+    match guess.cmp(&secret_number) {
+        // define what to do for each variant on separate arms
+        // each arm inclues pattern to match against (like Less) and what to do if true
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("Too big!"),
+        Ordering::Greater => println!("You win!"),
+    }
 }
